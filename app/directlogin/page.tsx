bad96b138f907d2a20f858de2d08ce2e01291bc3
saeed <saeedmaracy@gmail.com>
2026-06-23 09:44:40 +0330

    refrom query(convert currentindex to instagramerid) in directlogin

--- app/directlogin/page.tsx
+++ app/directlogin/page.tsx
@@ -1,5 +1,6 @@
 import DirectLoginClient from "brancy/components/signIn/directLoginClient";
 import { getInternalApiBaseUrl } from "brancy/helper/apiBaseUrl";
+import { IRefreshToken } from "brancy/models/interfaces";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -7,10 +8,10 @@ import { redirect } from "next/navigation";
 export default async function DirectLoginPage({
   searchParams,
 }: {
-  searchParams: Promise<{ bearer?: string; redirectUrl?: string; currentIndex?: string }>;
+  searchParams: Promise<{ bearer?: string; redirectUrl?: string; instagramerId?: string }>;
 }) {
-  const { bearer, redirectUrl, currentIndex } = await searchParams;
-  if (!bearer || !redirectUrl || !currentIndex) {
+  const { bearer, redirectUrl, instagramerId } = await searchParams;
+  if (!bearer || !redirectUrl || !instagramerId) {
     redirect("/");
   }
   const reqHeaders = await headers();
@@ -19,12 +20,13 @@ export default async function DirectLoginPage({
     cache: "no-store",
     headers: { "Content-Type": "application/json", Authorization: `Bearer ${bearer}` },
   });
-  if (res.status === 401 || res.status === 403) {
+  if (res.status === 401 || res.status === 403 || res.status === 500) {
     console.warn("Unauthorized or forbidden response from refresh token endpoint, redirecting to login.");
     redirect("/");
   }
-  const text = await res.json();
-  console.log("Direct login API response:", { status: res.status, text });
-  console.log("instagramerids:", text.value.role.instagramerIds);
-  return <DirectLoginClient res={text} redirectUrl={redirectUrl} currentIndex={currentIndex} />;
+  const response = await res.json();
+  const refreshToken = response.value as IRefreshToken;
+  console.log("Direct login API response:", { status: res.status, refreshToken });
+  console.log("instagramerids:", refreshToken.role);
+  return <DirectLoginClient res={refreshToken} redirectUrl={redirectUrl} instagramerId={Number(instagramerId)} />;
 }
