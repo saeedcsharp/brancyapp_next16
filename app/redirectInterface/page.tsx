447e1a1815fcde70ae5a768cbabc10f1fc9a03f9
saeed <saeedmaracy@gmail.com>
2026-06-28 13:18:52 +0330

    refrom redirectInterface and supportChat errors

--- app/redirectInterface/page.tsx
+++ app/redirectInterface/page.tsx
@@ -1,7 +1,5 @@
 import { redirect, notFound } from "next/navigation";
-
 const ALLOWED_DOMAINS = ["zarinpal.com", "podgetway.pod.ir", "stripe.com", "sandbox.zarinpal.com"];
-
 function isAllowedRedirectUrl(redirectUrl: string): boolean {
   try {
     const url = new URL(redirectUrl);
@@ -10,9 +8,12 @@ function isAllowedRedirectUrl(redirectUrl: string): boolean {
     return false; // اگه redirectUrl یه URL معتبر نباشه
   }
 }
-
-export default function RedirectInterfacePage({ searchParams }: { searchParams: { redirectUrl?: string } }) {
-  const redirectUrl = searchParams.redirectUrl;
+export default async function RedirectInterfacePage({
+  searchParams,
+}: {
+  searchParams: Promise<{ redirectUrl?: string }>;
+}) {
+  const { redirectUrl } = await searchParams;
   if (!redirectUrl || !isAllowedRedirectUrl(redirectUrl)) {
     notFound();
   }
