9753ec73c661db6bce54c087a8c5c3c48902333d
saeed <saeedmaracy@gmail.com>
2026-09-02 14:58:18 +0330

    refactor: reorganize imports and remove session check for improved layout functionality

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,22 +1,15 @@
 "use client";
 
-import { MouseEvent, useState } from "react";
-import { useRouter } from "next/router";
 import LeftUserHamMenue from "brancy/components/hambergurMenu/leftUserHamMenu";
 import UserPanelNavbar from "brancy/components/navbar/userPanelNavbar/userPanelNavbar";
 import UserSidebar from "brancy/components/sidebar/userSidebar/userSidebar";
 import SignOut from "brancy/components/signout/signOut";
 import SwitchAccount from "brancy/components/switchAccount/switchAccount";
-import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { MouseEvent, useState } from "react";
 
 export default function UserGroupLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const { data: session, status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      router.push("/");
-    },
-  });
   const newRoute = (router.asPath || "").split("?")[0].replaceAll("/", "");
   const [showLeftUserHamMenu, setShowLeftUserHamMenu] = useState(false);
   const [showNotifBar, setShowNotifBar] = useState(false);
@@ -63,7 +56,7 @@ export default function UserGroupLayout({ children }: { children: React.ReactNod
     setShowSignOut(false);
     setShowSwitch(false);
   };
-  if (status !== "authenticated") return null;
+
   return (
     <main className="marketAdsCart">
       <UserSidebar newRouth={newRoute} router={router} />
