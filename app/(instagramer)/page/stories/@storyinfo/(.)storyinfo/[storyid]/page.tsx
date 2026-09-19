4b2a83d754f0aa1d0982e402c4b4f5449e8acc93
saeed <saeedmaracy@gmail.com>
2026-09-19 08:49:06 +0330

    feat: implement role-based access control for story info page

--- app/(instagramer)/page/stories/@storyinfo/(.)storyinfo/[storyid]/page.tsx
+++ app/(instagramer)/page/stories/@storyinfo/(.)storyinfo/[storyid]/page.tsx
@@ -2,8 +2,11 @@
 
 import InterceptedRouteModal from "brancy/app/_compat/InterceptedRouteModal";
 import PageComponent from "../../../../../../../legacy-pages/page/stories/storyinfo";
+import { useSession } from "next-auth/react";
 
 export default function Page() {
+  const { status } = useSession();
+  if (status !== "authenticated") return null;
   return (
     <InterceptedRouteModal fallbackPath="/page/stories">
       <PageComponent />
