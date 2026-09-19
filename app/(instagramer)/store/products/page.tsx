643757473eccd88a540b78b1cf9b3f7074609986
saeed <saeedmaracy@gmail.com>
2026-09-19 08:49:57 +0330

    feat: enhance role-based access control for products page

--- app/(instagramer)/store/products/page.tsx
+++ app/(instagramer)/store/products/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import NotAllowed from "brancy/components/notOk/notAllowed";
 import NotAllowedShopper from "brancy/components/notOk/notAllowedShopper";
-import PageComponent from "../../../../legacy-pages/store/products/index";
+import { RoleAccess } from "brancy/helper/loadingStatus";
+import { PartnerRole } from "brancy/models/enums";
 import { useSession } from "next-auth/react";
+import PageComponent from "../../../../legacy-pages/store/products/index";
 export default function Page() {
-  const { data: session } = useSession();
-  if (session?.user.isInfluencer) return <NotAllowedShopper />;
+  const { data: session, status } = useSession();
+  if (status !== "authenticated" || !session) return null;
+  if (!RoleAccess(session, PartnerRole.Products)) {
+    return <NotAllowed />;
+  }
+  if (session.user.isInfluencer) return <NotAllowedShopper />;
   return <PageComponent />;
 }
