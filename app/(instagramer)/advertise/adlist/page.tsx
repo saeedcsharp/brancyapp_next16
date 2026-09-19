7ce44993add24f248de3997acc13393325df0884
saeed <saeedmaracy@gmail.com>
2026-09-19 08:48:36 +0330

    feat: implement role-based access control for advertising page

--- app/(instagramer)/advertise/adlist/page.tsx
+++ app/(instagramer)/advertise/adlist/page.tsx
@@ -4,10 +4,18 @@ import NotAllowedAdvertiser from "brancy/components/notOk/notAllowedAdvertiser";
 import { useSession } from "next-auth/react";
 import PageComponent from "../../../../legacy-pages/advertise/adlist";
 import Soon from "brancy/components/notOk/soon";
+import NotAllowed from "brancy/components/notOk/notAllowed";
+import { RoleAccess } from "brancy/helper/loadingStatus";
+import { PartnerRole } from "brancy/models/enums";
 
 export default function Page() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
+  if (status !== "authenticated" || !session) return null;
   return <Soon />;
+  if (!RoleAccess(session, PartnerRole.Ads)) {
+    return <NotAllowed />;
+  }
   if (session?.user.isShopper) return <NotAllowedAdvertiser />;
   return <PageComponent />;
 }
