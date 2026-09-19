3df74fa3500b6f2f438dbeed288759fa1afc7ce1
saeed <saeedmaracy@gmail.com>
2026-09-19 11:43:59 +0330

    feat: fix navbar visibility issues for message properties and subAdmin routes with pathname normalization

--- components/navbar/instagramerNavbar/navbarTabs.tsx
+++ components/navbar/instagramerNavbar/navbarTabs.tsx
@@ -328,11 +328,11 @@ const NavbarTabs = () => {
       indexValue: "message",
       initialSlide: 2,
     },
-    messageProperties: {
+    messageproperties: {
       id: "Properties",
       items: ["direct", "comments", "ticket", "Properties"],
       indexValue: "message",
-      initialSlide: 4,
+      initialSlide: 3,
     },
     walletpayment: {
       id: "payment",
@@ -414,11 +414,11 @@ const NavbarTabs = () => {
       indexValue: "setting",
       initialSlide: 0,
     },
-    settingsubAdmin: {
+    settingsubadmin: {
       id: "subAdmin",
       items: ["general", "subAdmin", "helpcenter"],
       indexValue: "setting",
-      initialSlide: 0,
+      initialSlide: 1,
     },
     settinghelpcenter: {
       id: "helpcenter",
