60ced05cccef75f621a2ba63094348e58a426dfd
saeed <saeedmaracy@gmail.com>
2026-09-19 08:52:12 +0330

    feat: implement role-based access control for product detail route

--- app/(instagramer)/store/products/productDetail/page.tsx
+++ app/(instagramer)/store/products/productDetail/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import { Suspense } from "react";
-import PageComponent from "../../../../../legacy-pages/store/products/productDetail";
 import NotAllowedShopper from "brancy/components/notOk/notAllowedShopper";
 import { useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { PartnerRole } from "brancy/models/enums";
+import NotAllowed from "brancy/components/notOk/notAllowed";
+import { Suspense } from "react";
+import PageComponent from "../../../../../legacy-pages/store/products/productDetail";
+import { RoleAccess } from "brancy/helper/loadingStatus";
 function ProductDetailRoute() {
   const { data: session, status } = useSession();
   const searchParams = useSearchParams();
   const tempId = searchParams.get("tempId");
   if (status !== "authenticated" || !session) return null;
   if (session.user.isInfluencer) return <NotAllowedShopper />;
+  if (!RoleAccess(session, PartnerRole.Products)) {
+    return <NotAllowed />;
+  }
   return <PageComponent tempId={tempId ?? ""} />;
 }
 
